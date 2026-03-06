3ad61e1f9f2a0fdc82c6a505597f07d32cf3d8bc
ChristianFerrer <ChristianFerrer@users.noreply.github.com>
2026-03-06 20:27:19 +0100

    config: stripe publishable key actualizada (test)

--- public/config.js
+++ public/config.js
@@ -7,5 +7,5 @@
 window.__CURIO_CONFIG__ = {
   supabaseUrl:          'https://gboguglxgvdsvzprtigj.supabase.co',
   supabaseAnon:         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdib2d1Z2x4Z3Zkc3Z6cHJ0aWdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0NDk4NDksImV4cCI6MjA4NzAyNTg0OX0.f1NcwM2D29kD3x-rFxiWH1kK6ME4SD294UxNygGE8-E',
-  stripePublishableKey: 'pk_test_REEMPLAZAR_CON_TU_CLAVE_PUBLICABLE',
+  stripePublishableKey: 'pk_test_51T7vjICvP4sDMGc3NWmx6YWOLOed4erUc1wnAWod3L4NiMOvzMxkzcrFiYyEmjaRRn13MJRuw8pa3zpG9FSJbMKr00wgS1J1NM',
 };
